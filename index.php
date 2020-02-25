807f9822577bb7ab458a81069218e59de73ca9ee
lihui <lihui300@qq.com>
2020-02-25 21:27:27 +0800

    2020年2月25日21:27:17

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 
 echo 'GITHUB & WEB服务器<br>';
 
-echo 'git ....';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, at. Deleniti totam neque tenetur recusandae sunt quas vel iusto autem et voluptatum, pariatur illum in possimus repudiandae hic quod id!
+</body>
+
+</html>
