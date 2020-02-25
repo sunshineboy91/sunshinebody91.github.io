33110f9aa3dcbf82c503d81077029350e6535b4d
lihui <lihui300@qq.com>
2020-02-25 22:19:29 +0800

    2020年2月25日22:19:24

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
 
 echo 'GITHUB & WEB服务器<br>';
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-</body>
-
-</html>
