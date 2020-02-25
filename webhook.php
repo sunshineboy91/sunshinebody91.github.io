0a3e11de36b9de6c8d67352cd09a8901b7ed20e7
lihui <lihui300@qq.com>
2020-02-25 22:17:58 +0800

    2020年2月25日22:17:54

--- webhook.php
+++ webhook.php
@@ -12,12 +12,11 @@ $path = "/www/wwwroot/git.lihuiyu.top";
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
 if ($signature) {
-  $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+  $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
-    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    echo shell_exec("cd {$path} && /usr/local/git/bin/git reset --hard origin/master && /usr/local/git/bin/git clean -f && /usr/local/git/bin/git pull 2>&1");
     exit();
   }
 }
 
 http_response_code(404);
-?>
