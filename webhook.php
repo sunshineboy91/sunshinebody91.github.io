fe01f5d1cbcfec62592f2feb2e899753039363aa
lihui <lihui300@qq.com>
2020-02-26 10:09:13 +0800

    commit

--- webhook.php
+++ webhook.php
@@ -14,7 +14,7 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
-    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    echo shell_exec("cd {$path} && /usr/local/git/bin/git reset --hard origin/master && /usr/local/git/bin/git clean -f && /usr/local/git/bin/git pull 2>&1");
     exit();
   }
 }
